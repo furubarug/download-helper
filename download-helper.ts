99443132e38f25f4b588c7c0a538e44ea537b42c
furubarug <83177512+furubarug@users.noreply.github.com>
2022-01-24 02:03:28 +0900

    実装都合で型定義ファイルを削除

--- download-helper.ts
+++ download-helper.ts
@@ -839,3 +839,20 @@ export class DownloadHelper {
             '</body></html>';
     }
 }
+
+declare const streamSaver: {
+    createWriteStream: (
+        fileName: string,
+        options?: {
+            size: null,
+            pathname: null,
+            writableStrategy: undefined,
+            readableStrategy: undefined
+        }
+    ) => WritableStream
+};
+
+declare const createWriter: new(underlyingSource: {
+    pull: (ctrl: { enqueue: (file: File) => void, close: () => void }) => Promise<void>
+}) => ReadableStream & { pull: () => void };
+
